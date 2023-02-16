128a6db6a1bb97548dc051578346e64ebd7b45a1
iantony254 <iantonyochola@gmail.com>
2023-02-16 09:42:56 -0800

    size.c correction 3

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,11 @@ int main(void)
 	long long alnglng;
 	float afloat;
 
-	printf("Size of a char: %lu byte(s)\n",sizeof(achar));
-	printf("Size of an int: %lu byte(s)\n",sizeof(aint));
-	printf("Size of a long int: %lu byte(s)\n",sizeof(along));
-	printf("Size of a long long int: %lu byte(s)\n",sizeof(alnglng));
-	printf("Size of a float: %lu byte(s)\n",sizeof(afloat));
+	printf("Size of a char: %lu byte(s)\n", sizeof(achar));
+	printf("Size of an int: %lu byte(s)\n", sizeof(aint));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(alnglng));
+	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
 
 	return	(0);
 }
