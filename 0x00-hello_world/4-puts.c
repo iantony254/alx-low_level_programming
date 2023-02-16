e6f1f99cf27a4b276c560e85a20b7057816b24a5
iantony254 <iantonyochola@gmail.com>
2023-02-16 09:26:07 -0800

    puts.c correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,9 @@
 
 /**
  * main is the entry point
+ *return 0(return value of 'main')
 */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
