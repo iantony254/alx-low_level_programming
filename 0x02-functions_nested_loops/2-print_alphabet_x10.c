718c3c89832106187e8041971a5f360855c73895
iantony254 <iantonyochola@gmail.com>
2023-02-21 09:03:19 -0800

    print alphabet x10 completed

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - function to print abc 10 times
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char ch, i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for(ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
