4733d7a0e7f4abd18d67a170b7a222d6bfd7562e
iantony254 <iantonyochola@gmail.com>
2023-02-21 10:47:19 -0800

    times table correct

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,7 +19,7 @@ void times_table(void)
 		{
 			i = (x * y);
 
-			if ((i / 10) >0)
+			if ((i / 10) > 0)
 			{
 				_putchar((i / 10) + '0');
 			}
@@ -27,7 +27,7 @@ void times_table(void)
 			{
 				_putchar(' ');
 			}
-			_putchar((i % 10 + '0');
+			_putchar((i % 10) + '0');
 
 			if (y < 9)
 			{
