1e47b328151a4c5a4463ef9075255077219f2738
iantony254 <iantonyochola@gmail.com>
2023-02-21 08:52:01 -0800

    print alphabet complete

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - function to print abc
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char abc;
+	
+	for (abc = 'a'; abc <= 'z'; abc++)
+	{
+		_putchar(abc);
+	}
+_putchar('\n');
+}
