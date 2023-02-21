a47394b18fdff6223454020cf6e1bc730d86f507
iantony254 <iantonyochola@gmail.com>
2023-02-21 08:35:54 -0800

    putchar complete

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - function to print
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ptchr[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+	unsigned int p;
+
+	for(p=0; p < sizeof(ptchr); p++)
+	{
+		_putchar(ptchr[p]);
+	}
+_putchar('\n');
+return (0);
+}
