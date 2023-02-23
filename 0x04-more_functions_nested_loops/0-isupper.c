d5550c909b4006adc010e288f18c517d950200e5
iantony254 <iantonyochola@gmail.com>
2023-02-23 13:53:12 +0300

    is upper test on vs code

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: character to be checked
+ * Return: 1 if true, 0 if false
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{	
+        return (1);
+    }
+	else 
+    {
+        return (0);
+    }
+}
