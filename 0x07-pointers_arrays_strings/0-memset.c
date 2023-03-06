09009fee1a0d3c0316a884cfe84bbea6c7c0eb48
iantony254 <iantonyochola@gmail.com>
2023-03-06 18:24:12 +0300

    rooot natty rooot

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,21 +2,22 @@
 #include "main.h"
 
 /**
- * _memset - check the code for Holberton School students.
- * @s: parameter for the array
- * @b: parameter for the values in the array
- * @n: parameter for the number of times to print the new values
- * Return: Always 0.
+ *_memset -  fill a block of memory with a specific value
+ *@s: starting address of memory to be filled
+ *@b: the desired value
+ *@n: number of bytes to be changed
+ *
+ *Return: changed array with new value for n bytes
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int a = 0;
+	int i=0;
 
-	while (a < n)
+	while  (n > 0)
 	{
-		s[a] = b;
-		a++;
+		s[i] = b;
+		i++;
+		n--;
 	}
-
 	return (s);
 }
